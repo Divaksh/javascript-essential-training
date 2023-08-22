7c44ca2bec147762cd958ae00b9b87524e21c666
Divaksh <me@divaksh.me>
2023-08-22 10:16:03 +0530

    Added new objects in given exercise file

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,54 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+/*
+    Keyboard Object
+*/
+
+const keyboard = {
+  type: "Mechanical",
+  keys: 104,
+  numLock: true,
+  backlit: false,
+  switch: {
+    type: "cherrymx",
+    color: "blue",
+    rgbValue: "FFFFFF",
+  },
+  toggleBacklit: function (value) {
+    this.backlit = value;
+  },
+  toggleNumLock: function (value) {
+    this.numLock = value;
+  },
+  changeLedColor: function (rgbColor) {
+    this.switch.rgbValue = rgbColor;
+  },
+};
+
+const laptop = {
+  brand: "Asus",
+  memorySlot: 1,
+  memory: {
+    brand: "Kingston",
+    size: 12,
+    channel: "Single",
+  },
+  storageCount: 1,
+  storage: {
+    brand: "WD",
+    size: 1024,
+    type: "SSD",
+  },
+  processor: {
+    brand: "AMD",
+    model: "Ryzen™ 7 5800X",
+    socket: "AM4",
+  },
+  chnageStorage: function (brand, size, type) {
+    this.storage.brand = brand;
+    this.storage.size = size;
+    this.storage.type = type;
+  },
+};
